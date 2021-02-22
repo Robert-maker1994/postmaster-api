1e7fe460179e576497bbfcb2ae0cf05f49d166f8
Robert Lawson <63192607+Robert-maker1994@users.noreply.github.com>
2021-02-22 08:43:38 +0000

    Update controller-featured.js

--- Controllers/controller-featured.js
+++ Controllers/controller-featured.js
@@ -15,7 +15,7 @@ try {
            //Selecting the DB and the Collection 
            const db = await client.db("dev").collection("postData"); 
            //Getting only featured posts
-                const Featured = await db.find({"blogCategory": "Featured"}).sort({ name: 1}).toArray().then(Feat => {
+                const Featured = await db.find({"category": "Featured"}).sort({ name: 1}).toArray().then(Feat => {
                 Feat.forEach(console.log);
                 return FeaturedPosts = Feat
                     }).catch(err => console.error(`Failed to find Featured Posts: ${err}`));
