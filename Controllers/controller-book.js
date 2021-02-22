e87797b373e69cdcfc208e125e2713800705374d
Robert Lawson <63192607+Robert-maker1994@users.noreply.github.com>
2021-02-22 08:44:44 +0000

    Update controller-book.js

--- Controllers/controller-book.js
+++ Controllers/controller-book.js
@@ -13,7 +13,7 @@ exports.book  = async (req, res) => {
       //Selecting the DB and the Collection 
       const db = await client.db("dev").collection("postData"); 
        //Getting only featured posts
-       const review = await db.find({"blogCategory": "Book Summary"}).sort({ name: 1}).toArray().then(book => {
+       const review = await db.find({"category": "Book Summary"}).sort({ name: 1}).toArray().then(book => {
         book.forEach(console.log)
         return BookPosts = book
             }).catch(err => console.error(`Failed to find Featured Posts: ${err}`));
